05129499b804efe2221d205a83bdcdcec06eed05
chasingdonis <cuixianning@hotmail.com>
2015-11-12 16:21:55 -0600

    Edit main.js

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
-$(function() {
-   $("div.event").hover(alter);    
-});
-    
-function alter() {
-   $(this).toggleClass("altered");
-}
+ $(function() {
+            setInterval("rotateImages()", 3000);
+        });
 
+        function rotateImages() {
+            var oCurPhoto = $('#hero div.current');
+            var oNxtPhoto = oCurPhoto.next();
+            if (oNxtPhoto.length == 0)
+                oNxtPhoto = $('#hero div:first');
+
+            oCurPhoto.removeClass('current').addClass('previous');
+            oNxtPhoto.css({ opacity: 0.0 }).addClass('current')
+                    .animate({ opacity: 1.0 }, 1000,
+                                function() {
+                                    oCurPhoto.removeClass('previous');
+                                });
+        }
 
 
